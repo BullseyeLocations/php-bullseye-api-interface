d88bac35f969d0b83aa41e2ddd6ab05137da1b46
Carlos Guzman <caralbgm@gmail.com>
2017-04-02 12:51:07 -0500

    Addition of ChangePassword method in User module.

--- Bullseye.php
+++ Bullseye.php
@@ -5,6 +5,7 @@ require_once "Connection.php";
 include_once "modules/Location.php";
 include_once "modules/Search.php";
 include_once "modules/Lead.php";
+include_once "modules/User.php";
 
 /**
  * Front controller to handle all requests make to Bullseye API. This class loads other modules
@@ -255,5 +256,14 @@ class Bullseye{
    */
   function authenticateClient(){
     return $this->connection->process_query(Lead::$actions['AuthenticateClient']);
+  }
+  
+  /**
+   * https://bullseyelocations.readme.io/v1.0/reference#changepassword
+   *
+   * User module.
+   */
+  function changePassword($userName, $newPassword){
+    return $this->connection->process_query(User::$actions['ChangePassword'], compact('userName', 'newPassword'));
   }
 }
---
+++ modules/User.php
@@ -0,0 +1,24 @@
+<?php
+namespace Bullseye;
+
+/**
+ * Handle requests to user module in Bullseye REST API.
+ * http://api.bullseyelocations.com/services/user-web-service
+ */
+class User{
+
+  /**
+   * Meta data to make requests of Lead module.
+   */
+  static $actions = array(
+  
+    /**
+     * https://bullseyelocations.readme.io/v1.0/reference#changepassword
+     */
+    'ChangePassword' => array(
+      'httpMethod' => 'post',
+      'action' => 'RestUser.svc/ChangePassword',
+    ),
+  );  
+}
+
